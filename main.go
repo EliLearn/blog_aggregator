dd57fc4752da43cae1d6485c57c256c841f9a656
Eli Wilson <eli.techlearn@gmail.com>
2023-06-13 16:17:57 -0400

    sketchy addtion of vendor packages

--- main.go
+++ main.go
@@ -3,22 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println ("hello world")
+	godotenv.Load() // load enviornmental variables
+	fmt.Println ("\n Time for learning") // necessary encouraging message
 
-	godotenv.Load()
-
-	r := chi.NewRouter()
 
 	portString := os.Getenv("PORT")
+
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
-	fmt.Println("Port:", portString)
+	//fmt.Println("Port:", portString)
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+	
+	log.Printf("Server starting on Port: %v", portString)
+	//return an error if problem with handling http request
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router.Use(cors.Handler(cors.Options{}))
+	
+	
 }
